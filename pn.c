cdc06c2af555e5879409bef6f862b9f8a8d1eb86
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-01-09 11:00:15 +0000

    Create pn.c

---
+++ pn.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+{
+printf("no is positive");
+}
+else if(a<0)
+{
+printf("no is negative");
+}
+else
+{
+printf("zero");
+}
+return 0;
+}
