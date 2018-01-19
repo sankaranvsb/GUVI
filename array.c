4f490282bee1d7553a6d25d407216def20044125
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-01-19 16:14:06 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int N,K,a[20],i,sum=0;
+scanf("%d%d",&N,&K);
+for(i=0;i<N;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<K;i++)
+{
+sum=sum+a[i];
+}
+printf("%d",sum);
+}
