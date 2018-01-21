c2ad79e0a502b354e3bb5929a63592ff5898c17e
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-01-21 13:56:34 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 int main()
 {
-int rem,sum,rks,num
+int rem,sum=0,rks,num;
 scanf("%d",&num);
+rks=num;
 while(num>0)
 {
 rem=num%10;
-sum=rem*rem*rem';
+sum=sum*10+rem;
 num=num/10;
+}
+  if(sum==rks)
+    printf("palindrome");
+  else
+    printf("not palindrome");
+return 0;
 }
