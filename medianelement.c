1148205ab9b0224b9b28685cbf6ce6a71fe0087f
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-01-26 14:31:08 +0530

    Create medianelement.c

---
+++ medianelement.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void) 
+{
+int a[5],i,j,temp;
+for(i=0;i<3;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<3;i++)
+{
+for(j=i+1;j<3;j++)
+{
+	if(a[i]>a[j])
+{
+	temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+}}}
+
+
+printf("%d",a[1]);
+
+    return 0;
+}
