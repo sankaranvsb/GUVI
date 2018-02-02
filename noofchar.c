64871e0b84bb27851287c128884b7d4e0c2c69ab
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-02 15:10:49 +0530

    Update noofchar.c

--- noofchar.c
+++ noofchar.c
@@ -4,16 +4,16 @@ int main()
 {
 char a[55];
 int b,count=0,i,num=0,no=0;
-scanf("%s",&a);
+gets(a);
   for(i=0;a[i]!='\0';i++)
-  {++no;
+  {no++;
   }
 for(i=0;a[i]!='\0';i++)
-{if(a[i]==" ")
+{if(a[i]==' ')
 {
 count++;
 num=no-count;
-};
+}
 }
 printf("%d",num);
 return 0;
