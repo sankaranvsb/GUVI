75c5b776f5612d0b222578e1242b3c5e132635ee
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-02 15:07:18 +0530

    Update noofspces.c

--- noofspces.c
+++ noofspces.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {char a[100];
-int count,i;
-scanf("%s",&a);
-for(i=0;i!='\0';i++)
+int count=0,i;
+gets(a);
+for(i=0;a[i]!='\0';i++)
 {
-if(a[i]==" ")
+if(a[i]==' ')
 {count++;
 }}
 printf("%d",count);
