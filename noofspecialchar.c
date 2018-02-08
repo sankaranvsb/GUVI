7cdf79478215641a6d302b31a14d133f3e8e96a1
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-08 15:04:49 +0530

    Update noofspecialchar.c

--- noofspecialchar.c
+++ noofspecialchar.c
@@ -4,11 +4,11 @@ int main()
 {
 int no,c=0,b=0,splfun=0;
 char a[100];
-gets(a);
+scanf("%s",&a);
 no=strlen(a);
 for(int i=0;a[i]<no;i++)
 {
-if{a[i]=>'0' && a[i]<='9')
+if(a[i]=>'0' && a[i]<='9')
 {
   c++;
 }
