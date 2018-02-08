fc727af27982c2f79565a4d7e4a9b2fc17d3ff62
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-08 14:13:53 +0530

    Update noofspecialchar.c

--- noofspecialchar.c
+++ noofspecialchar.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 #include<string.h>
-int main
+int main()
 {
-int no,a=0,b=0,splfun=0;
-char a[1000];
+int no,c=0,b=0,splfun=0;
+char a[100];
 gets(a);
 no=strlen(a);
-for(i=0;a[i]<no;i++)
+for(int i=0;a[i]<no;i++)
 {
 if{a[i]=>'0' && a[i]<='9')
-{a++;
+{
+  c++;
 }
 else if((a[i]>'a' && a[i]<'z') || (a[i]>'A'  && a[i]<'Z'))
 {
 b++;
 }
 else
-{splfun++;
+{
+  splfun++;
 }
+  }
 printf("%d",splfun);
 return 0;
 }
