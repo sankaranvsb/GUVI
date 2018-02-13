7e718c410fa1e11338210f4063ffaea7d9ac0e64
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-13 14:51:52 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int n,count=0,k;
+scanf("%d",&n);
+n=k;
+for(int i=2;i<n;i++)
+{ 
+if(i%2==0)
+{
+count++;
+}
+}
+if(n==k)
+{
+printf("prime");
+}
+else
+{
+printf(" not prime");
+}
+return 0;
+}
