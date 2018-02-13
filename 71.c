51a977f8f631c5c5ffe004833866e44c2885b44e
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-13 15:12:02 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{int no;
+char a[20],b[20];
+scanf("%s",&a);
+strcpy(b,a);
+strrev(a);
+no=strcmp(a,b);
+if(no==1)
+{
+printf("palindrome");
+}
+else
+{
+printf("not palindrome");
+}
+return 0;
+}
