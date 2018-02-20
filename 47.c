1517ed7eb90af8c16d6fcc106b451cc7f3b6e187
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-20 11:26:16 +0000

    Update 47.c

--- 47.c
+++ 47.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int a[10000],i,n,temp=0,j;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("%d\n",a[0]);
+printf("%d",a[n-1]);
+return 0;
+} 
 
