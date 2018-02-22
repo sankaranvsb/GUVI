bdd6e329405639bda17dab486f25324c233f0400
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-22 16:14:30 +0530

    Create 60.c

---
+++ 60.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int k,n,temp=0,r=0,s=1;
+scanf("%d",&n);
+printf("%d\t%d",r,s)
+for(k=2;k<n;k++)
+{
+temp=r+s;
+r=s;
+s=temp;
+printf("%d\t",temp);
+}
+return 0;
+} 
+
