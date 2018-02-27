a6f0abc594d6aa58461ad0c9e6c5be8bcd609d98
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-02-27 16:16:51 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{int no,a,b,count=0;
+printf("enter the no");
+scanf("%d",&no);
+printf("\nenter the limit");
+scanf("%d\t%d",&a,&b);
+for(int i=a;i<=b;i++)
+{
+if(i==no)
+{
+count=1;
+}
+}
+if(count==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
