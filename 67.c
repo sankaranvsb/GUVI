b026302306142bef7804e3896c699880c7350697
sankaranvsb <31505537+sankaranvsb@users.noreply.github.com>
2018-03-01 16:06:30 +0530

    Update 67.c

--- 67.c
+++ 67.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 int main()
 {
-int no,a=1,rem=0;
-scanf("%d",&no);
-if(no>0 && no<10)
+int n,a=1,rem=0;
+scanf("%d",&n);
+if(n>0 && n<10)
 {
 printf("10");
 }
 else
 {
-no=no/10;
-rem=no%10;
+n=n/10;
+rem=n%10;
 if(rem>=1 &&rem<=5)
 {
 a=a+2;
-no=a*10;
-printf("%d",no);
+n=a*10;
+printf("%d",n);
 }
 else
 {
 a++;
-no=a*10;
-printf("%d",no);
+n=a*10;
+printf("%d",n);
 }}
 return 0;
 }
